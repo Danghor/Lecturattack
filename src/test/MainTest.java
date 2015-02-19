77ba222fb7de2b9a91d08cd417af1fb207c8a824
Danghor <danghor+github.com@gmail.com>
2015-02-19 08:13:01 +0100

    add java test file to test proper slick2D installation

---
+++ src/test/MainTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class MainTest extends BasicGame {
+	public MainTest() {
+		super("SimpleTest");
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		g.drawString("Hello, Slick world!", 0, 100);
+	}
+
+	public static void main(String[] args) {
+		try {
+			AppGameContainer app = new AppGameContainer(new MainTest());
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+}
