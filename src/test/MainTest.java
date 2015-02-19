f70e2dc3677d666c96057385a8b88842cbce39d2
Tim Adamek <tadamek94@gmail.com>
2015-02-19 14:41:11 +0100

    Small change to positioning of text

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import org.lwjgl.util.Rectangle;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
 
 public class MainTest extends BasicGame {
 	public MainTest() {
@@ -23,7 +26,7 @@ public class MainTest extends BasicGame {
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-		g.drawString("Hello, Slick world!", 0, 100);
+		g.drawString("Hello, Slick world!", System.nanoTime()%0x191, System.nanoTime()%0x1c1);
 	}
 
 	public static void main(String[] args) {
