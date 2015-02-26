eef30849fce59875ec7702653f84643d17a48889
Danghor <danghor+github.com@gmail.com>
2015-02-26 13:30:11 +0100

    implement first gravity prototype

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -4,9 +4,18 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.ShapeFill;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.fills.GradientFill;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.gui.TextField;
 
 public class MainTest extends BasicGame {
+	private Rectangle myRect = new Rectangle(80, 30, 50, 50);
+	private float gravity = 1.0f;
+	private float currentYVelocity = 0;
+	private long count = 0;
+
 	public MainTest() {
 		super("SimpleTest");
 	}
@@ -18,12 +27,23 @@ public class MainTest extends BasicGame {
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException {
+		count++;
+		if (count % 35 == 0) { // nur zum Test, später würde ich über Ticks
+								// gehen (oder TargetFPS)
+			myRect.setY(myRect.getY() - currentYVelocity);
+			currentYVelocity -= gravity;
+		}
+
+		if (count % 1500 == 0) {
+			currentYVelocity = 0;
+			myRect = new Rectangle(80, 30, 50, 50);
+		}
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-		g.drawString("Hello, Slick world!", 0, 100);
+		g.fill(myRect);
 	}
 
 	public static void main(String[] args) {
