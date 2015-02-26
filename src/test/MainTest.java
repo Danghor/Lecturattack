3f6cd99b5137f5dc634cf76019ce1bce9d7fb8f1
Danghor <danghor+github.com@gmail.com>
2015-02-26 13:42:19 +0100

    fix gravity test

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,20 +1,11 @@
 package test;
 
-import org.lwjgl.util.Rectangle;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.ShapeFill;
 import org.newdawn.slick.SlickException;
-<<<<<<< HEAD
-import org.newdawn.slick.fills.GradientFill;
 import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.gui.TextField;
-=======
-import org.newdawn.slick.geom.Shape;
-import org.newdawn.slick.geom.Transform;
->>>>>>> refs/remotes/origin/master
 
 public class MainTest extends BasicGame {
 	private Rectangle myRect = new Rectangle(80, 30, 50, 50);
@@ -36,7 +27,7 @@ public class MainTest extends BasicGame {
 		count++;
 		if (count % 35 == 0) { // nur zum Test, später würde ich über Ticks
 								// gehen (oder TargetFPS)
-			myRect.setY(myRect.getY() - currentYVelocity);
+			myRect.setY((int) (myRect.getY() - currentYVelocity));
 			currentYVelocity -= gravity;
 		}
 
@@ -49,12 +40,7 @@ public class MainTest extends BasicGame {
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-<<<<<<< HEAD
 		g.fill(myRect);
-=======
-		
-		g.drawString("Hello, Slick world!", System.nanoTime()%0x191, System.nanoTime()%0x1c1);
->>>>>>> refs/remotes/origin/master
 	}
 
 	public static void main(String[] args) {
