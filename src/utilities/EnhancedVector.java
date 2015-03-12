4d35070ebe17ad4d3eb5fefa84f5dcc97eac8151
Danghor <danghor+github.com@gmail.com>
2015-03-12 22:10:41 +0100

    add utilities package and define some field for RigidBody

---
+++ src/utilities/EnhancedVector.java
@@ -0,0 +1,14 @@
+package utilities;/*
+ * Copyright (c) 2015.
+ */
+
+import org.newdawn.slick.geom.Vector2f;
+
+/**
+ * Created by Nick Steyer on 12/03/2015
+ */
+public class EnhancedVector extends Vector2f {
+  public float scalarCrossProduct(EnhancedVector partner) {
+    return 0f;
+  }
+}
