0b9417aacbb386a682f7bed070acc7691530ce16
Danghor <danghor+github.com@gmail.com>
2015-03-15 21:34:55 +0100

    implement crossProduct and rotation for EnhancedVector

--- src/utilities/EnhancedVector.java
+++ src/utilities/EnhancedVector.java
@@ -8,7 +8,45 @@ import org.newdawn.slick.geom.Vector2f;
  * Created by Nick Steyer on 12/03/2015
  */
 public class EnhancedVector extends Vector2f {
-  public float scalarCrossProduct(EnhancedVector partner) {
-    return 0f;
+  public EnhancedVector(float x, float y) {
+    super(x, y);
+  }
+
+  /**
+   * @param partner The EnhancedVector with which the cross product is to be calculated.
+   *
+   * @return The scalar value of the cross product.
+   */
+  public float crossProductScalar(EnhancedVector partner) {
+    return (this.x * partner.y - this.y * partner.x);
+  }
+
+  /**
+   * Rotates the vector by a specified angle around a specified center.
+   *
+   * @param angle  The angle used to rotate the vector. Positive angle: clockwise rotation; Negative angle: counterclockwise rotation
+   * @param center The center around which the vector is to be rotated.
+   */
+  public void rotate(float angle, EnhancedVector center) {
+
+    //move center to origin of coordinate system
+    double tmpX = this.x - center.x;
+    double tmpY = this.y - center.y;
+
+    //rotate x and y using the origin as the center
+    double rotatedTmpX = Math.cos(angle) * tmpX - Math.sin(angle) * tmpY;
+    double rotatedTmpY = Math.sin(angle) * tmpX + Math.cos(angle) * tmpY;
+
+    //move center to its original position
+    double newX = rotatedTmpX + center.x;
+    double newY = rotatedTmpY + center.y;
+
+    //apply the calculated values, i.e. rotate the vector
+    this.x = (float) newX;
+    this.y = (float) newY;
+  }
+
+  public EnhancedVector getScaled(float factor) {
+    return (EnhancedVector) super.scale(factor);
   }
 }
