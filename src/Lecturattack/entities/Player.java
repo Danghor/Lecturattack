43f3947187c4a2cd8616aaf0341448367321e1d7
Andi <andreasgeis94@gmail.com>
2015-03-31 12:20:23 +0200

    added powerSlider

--- src/Lecturattack/entities/Player.java
+++ src/Lecturattack/entities/Player.java
@@ -1,26 +1,35 @@
 package Lecturattack.entities;
+
 /*
  * Copyright (c) 2015.
  */
 
 import Lecturattack.utilities.xmlHandling.configLoading.PlayerStandard;
+
+import org.newdawn.slick.AngelCodeFont;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * @author Tim Adamek
  * @author Nick Steyer
+ * @author Andreas Geis
  */
 public class Player implements Renderable {
 
-  //TODO remove
+  // TODO remove
   float strength = 70;
   private Image bodyImage;
   private Image armImage;
-  private boolean isThrowing;
+
+  public enum PlayerState {
+    ANGLE_SELECTION, POWER_SLIDER, THROWING
+  }
+
   private Projectile projectile;
   private PowerSlider powerSlider;
   private float positionX;
@@ -30,11 +39,10 @@ public class Player implements Renderable {
   private ProjectileMeta projectileMeta;
   private float directionAngle;
 
+  private PlayerState playerState;
 
-
-  private float armShoulderX;//must be set in relation to player
+  private float armShoulderX;// must be set in relation to player
   private float armShoulderY;
-
 
   public Player(Image bodyImage, Image armImage, ProjectileMeta projectileMeta) {
     this.positionX = 0f;
@@ -44,38 +52,49 @@ public class Player implements Renderable {
     this.projectileMeta = projectileMeta;
     reset();
 
-    //set the position of the projectile to be on the hand
-    this.projectilePositionX=80;
-    this.projectilePositionY=195;
+    // set the position of the projectile to be on the hand
+    this.projectilePositionX = 80;
+    this.projectilePositionY = 195;
 
   }
 
   public void setPosition(float x, float y) {
     positionX = x;
     positionY = y;
-    //the position of the arm must be set in relation to the player
-    armShoulderX=x-36;
-    armShoulderY=y+10;
+    // the position of the arm must be set in relation to the player
+    armShoulderX = x - 36;
+    armShoulderY = y + 10;
   }
 
   public void reset() {
-    isThrowing = false;
+    playerState = PlayerState.ANGLE_SELECTION;
     projectile = new Projectile(projectileMeta, 0f, 0f);
+    powerSlider = new PowerSlider();
   }
 
   public void moveArm(float degreeDifference) {
-    //todo: check if movement possible, turn arm etc.
-    if (!isThrowing) {
+    // todo: check if movement possible, turn arm etc.
+    if (playerState == PlayerState.ANGLE_SELECTION) {
       this.directionAngle += degreeDifference;
-      projectilePositionX= ((float) Math.cos(Math.toRadians(directionAngle)+Math.PI/4) * strength)+armShoulderX+85;
-      projectilePositionY= ((float) Math.sin(Math.toRadians(directionAngle)+Math.PI/4) * strength)+armShoulderY+135;
+      projectilePositionX = ((float) Math.cos(Math.toRadians(directionAngle) + Math.PI / 4) * strength) + armShoulderX + 85;
+      projectilePositionY = ((float) Math.sin(Math.toRadians(directionAngle) + Math.PI / 4) * strength) + armShoulderY + 135;
     }
   }
 
-  public final Projectile throwProjectile(float strength) {
-    isThrowing = true;
-    //TODO apply force to projectile
-    return projectile;
+  /**
+   * lock the current Selection (setAngle or setPower) and increment the
+   * playerState do nothing if the player already threw the projectile
+   * @return
+   */
+  public final Projectile throwProjectile() {
+    if (playerState == PlayerState.ANGLE_SELECTION) {
+      playerState = PlayerState.POWER_SLIDER;
+      return null;
+    } else {
+      playerState = PlayerState.THROWING;
+      // TODO apply force to projectile using powerSlider.getForce();
+      return projectile;
+    }
   }
 
   @Override
@@ -85,10 +104,20 @@ public class Player implements Renderable {
     graphics.drawImage(bodyImage, positionX, positionY);
     graphics.drawImage(armImage, armShoulderX, armShoulderY);
 
-    if (!isThrowing) {
-      //todo: set position to middle of the player's hand
-      projectile.setCenterPosition(projectilePositionX,projectilePositionY);
+    if (playerState != PlayerState.THROWING) {
+      // todo: set position to middle of the player's hand
+      projectile.setCenterPosition(projectilePositionX, projectilePositionY);
       projectile.render(gameContainer, stateBasedGame, graphics);
+    }
+    
+    if(playerState == PlayerState.POWER_SLIDER || playerState == PlayerState.THROWING){
+      powerSlider.render(gameContainer, stateBasedGame, graphics);
+    }
+  }
+
+  public void update(int delta) {
+    if (playerState == PlayerState.POWER_SLIDER) {
+      powerSlider.update(delta);
     }
   }
 }
