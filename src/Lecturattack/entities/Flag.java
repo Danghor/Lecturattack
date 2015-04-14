871181a8420f8a3f16b77768e3c37fe4e3c07764
Tim Adamek <tadamek94@gmail.com>
2015-04-14 11:51:25 +0200

    Changed position of flag and flag is now drawn as a filled triangle

--- src/Lecturattack/entities/Flag.java
+++ src/Lecturattack/entities/Flag.java
@@ -2,8 +2,11 @@ package Lecturattack.entities;/*
  * Copyright (c) 2015.
  */
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.ShapeFill;
+import org.newdawn.slick.fills.GradientFill;
 import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -13,17 +16,17 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Flag implements Renderable {
   private float windScale;
   public void setWindScale(float windScale){
-    this.windScale=windScale;
+    this.windScale=windScale*100;
   }
 
   @Override
   public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) {
     Polygon triangle = new Polygon();
-    triangle.addPoint(100,200);
-    triangle.addPoint(100,300);
-    triangle.addPoint(200*windScale,250);
+    triangle.addPoint(gameContainer.getWidth()/2,10);
+    triangle.addPoint(gameContainer.getWidth()/2,70);
+    triangle.addPoint(gameContainer.getWidth()/2 + windScale,40);
+
+    graphics.fill(triangle);
     graphics.draw(triangle);
-
-
   }
 }
