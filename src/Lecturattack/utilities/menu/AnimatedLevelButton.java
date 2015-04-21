a053efd187318c7b112b574f7febe40e184a425b
Danghor <danghor+github.com@gmail.com>
2015-04-21 17:41:32 +0200

    delete unnecessary method

--- src/Lecturattack/utilities/menu/AnimatedLevelButton.java
+++ src/Lecturattack/utilities/menu/AnimatedLevelButton.java
@@ -1,10 +1,7 @@
 package Lecturattack.utilities.menu;
 
 import Lecturattack.entities.Renderable;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * AnimatedLevelButton inherits from AnimatedButton.
@@ -23,10 +20,5 @@ public class AnimatedLevelButton extends AnimatedButton implements Renderable {
    */
   public AnimatedLevelButton(int x, int y, Image activeImage, Image inactiveImage) {
     super(x, y, activeImage, inactiveImage);
-  }
-
-  @Override
-  public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) {
-    super.render(gameContainer, stateBasedGame, graphics);
   }
 }
