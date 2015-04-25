930c726ac57c74b6274abe02707c5b2ffb775d0a
Tim Adamek <tim.adamek@web.de>
2015-04-25 21:18:18 +0200

    Wrote wind on flag

--- src/Lecturattack/entities/Flag.java
+++ src/Lecturattack/entities/Flag.java
@@ -18,11 +18,12 @@ public class Flag implements Renderable {
   @Override
   public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) {
     Polygon triangle = new Polygon();
-    triangle.addPoint(gameContainer.getWidth() / 2, 10);
-    triangle.addPoint(gameContainer.getWidth() / 2, 70);
-    triangle.addPoint(gameContainer.getWidth() / 2 + windScale, 40);
+    triangle.addPoint(gameContainer.getWidth() / 2, 20);
+    triangle.addPoint(gameContainer.getWidth() / 2, 80);
+    triangle.addPoint(gameContainer.getWidth() / 2 + windScale, 50);
     graphics.fill(triangle, new GradientFill(gameContainer.getWidth() / 2, 40f, new Color(0x6E, 0x0D, 0x13), gameContainer.getWidth() / 2 + windScale, 40f, new Color(0xCB, 0x32, 0x2C)));
     graphics.draw(triangle);
+    graphics.drawString("Wind", gameContainer.getWidth() / 2 - 20, 3);
   }
 
   public void setWindScale(float windScale) {
